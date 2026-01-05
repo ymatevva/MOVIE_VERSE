4c71a982dde040826626de0281bc343a4b4d3f38
ymatevva <yoanamatevva@gmail.com>
2026-01-05 12:51:12 +0200

    Add validation for login and registration

--- js/login.js
+++ js/login.js
@@ -1,22 +1,69 @@
+
 const MIN_LENGTH = 8;
 
 const inputPass = document.getElementById("password");
+const username = document.getElementById("username");
 const loginForm = document.getElementById("loginForm")
+ 
+let isPassValid = false;
+
+function getUsers() {
+    return JSON.parse(localStorage.getItem("users")) || [];
+}
+
+function isUsernameInSystem(username) {
+    
+    const users = getUsers();
+
+    for (let i = 0; i < users.length; i++) {
+        if(username === users[i].username) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isPasswordMatch(password, username) {
+         
+    const users = getUsers();
+
+    for (let i = 0; i < users.length; i++) {
+        if(username === users[i].username) {
+            return password === users[i].password;
+        }
+    }
+    return false;
+}
 
 inputPass.addEventListener("input", () => {
     if(inputPass.value.length >= MIN_LENGTH) {
-        inputPass.classList.remove("input-invalid");
+       inputPass.classList.remove("input-invalid");
        inputPass.classList.add("input-valid");
+       isPassValid = true;
     } else {
-         inputPass.classList.remove("input-valid");
+       inputPass.classList.remove("input-valid");
        inputPass.classList.add("input-invalid");
+       isPassValid = false;
     }
 });
 
 
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (inputPass.value.length >= MIN_LENGTH) {
+    
+    if (isPassValid) {
+       const usernameV = username.value.trim();
+       const passV = inputPass.value;
+       if (!isUsernameInSystem(usernameV)) {
+        alert("The user is not registered in the system yet.");
+        return;
+       } 
+
+       if (!isPasswordMatch(passV, usernameV)) {
+        alert("The password for the given username is not correct.");
+        return;
+       }
+
        window.location.href = "catalogue.html";
     } else {
         alert('Please enter correct data.');
--- js/registration.js
+++ js/registration.js
@@ -4,10 +4,14 @@ const registrationForm = document.getElementById("registration-form");
 const emailInput = document.getElementById("email");
 const inputPass = document.getElementById("password");
 const birthdateInput = document.getElementById("birthdate");
+const confirmPass = document.getElementById("confirm-password");
+const passVerMess = document.getElementById("pass-verification");
+const username = document.getElementById("username");
 
 let isEmailValid = false;
 let isPasswordValid = false;
 let isBirthdateValid = false;
+let isConfirmPassSame = false;
 
 emailInput.addEventListener("input", () => {
     if (emailInput.value.includes('@')) {
@@ -23,42 +27,106 @@ emailInput.addEventListener("input", () => {
 
 
 inputPass.addEventListener("input", () => {
-    if(inputPass.value.length >= MIN_LENGTH) {
+    if (inputPass.value.length >= MIN_LENGTH) {
         inputPass.classList.remove("input-invalid");
-       inputPass.classList.add("input-valid");
-       isPasswordValid = true;
+        inputPass.classList.add("input-valid");
+        isPasswordValid = true;
     } else {
-         inputPass.classList.remove("input-valid");
-       inputPass.classList.add("input-invalid");
-       isPasswordValid = false;
+        inputPass.classList.remove("input-valid");
+        inputPass.classList.add("input-invalid");
+        isPasswordValid = false;
     }
 });
 
 
 birthdateInput.addEventListener("input", () => {
-   
+
     const birthD = new Date(birthdateInput.value);
     const currD = new Date();
     const userAge = currD.getFullYear() - birthD.getFullYear();
-    const months = currD.getMonth() - birthD.getMonth();
 
-    if(userAge >= 12 && months >= 0 && currD.getDate() >= birthD.getDate()) {
+    if (userAge >= 12) {
         birthdateInput.classList.remove("input-invalid");
-       birthdateInput.classList.add("input-valid");
-       isBirthdateValid = true;
+        birthdateInput.classList.add("input-valid");
+        isBirthdateValid = true;
     } else {
-         birthdateInput.classList.remove("input-valid");
-       birthdateInput.classList.add("input-invalid");
-       isBirthdateValid = false;
+        birthdateInput.classList.remove("input-valid");
+        birthdateInput.classList.add("input-invalid");
+        isBirthdateValid = false;
     }
 });
+
+
+confirmPass.addEventListener("input", () => {
+
+    if (confirmPass.value === inputPass.value) {
+        confirmPass.classList.remove("input-invalid");
+        confirmPass.classList.add("input-valid");
+        passVerMess.textContent = "Passwords match.";
+        isConfirmPassSame = true;
+    } else {
+        confirmPass.classList.remove("input-valid");
+        confirmPass.classList.add("input-invalid");
+        passVerMess.textContent = "Passwords do not match.";
+        isConfirmPassSame = false;
+    }
+});
+
+function getUsers() {
+    return JSON.parse(localStorage.getItem("users")) || [];
+}
+
+
+function isUserNameInSystemAlready(username) {
+
+    let currUsers = getUsers();
+    for (let index = 0; index < currUsers.length; index++) {
+        if (username === currUsers[index].username) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isEmailInSystemAlready(email) {
+
+    let currUsers = getUsers();
+    for (let index = 0; index < currUsers.length; index++) {
+        if (email === currUsers[index].email) {
+            return true;
+        }
+    }
+    return false;
+}
 
 
 registrationForm.addEventListener("submit", (e) => {
 
     e.preventDefault();
-    if(isBirthdateValid && isEmailValid && isPasswordValid) {
-        window.location.href="catalogue.html";
+
+    if (isBirthdateValid && isEmailValid && isPasswordValid && isConfirmPassSame) {
+
+        const usernameV = username.value.trim();
+        const emailV = emailInput.value.trim();
+        
+        if(isUserNameInSystemAlready(usernameV)){
+           alert("Username is already in the system.");
+           return;
+        }
+
+        if (isEmailInSystemAlready(emailV)) {
+            alert("Email is already in the system.");
+            return;
+        }
+
+        const passV = inputPass.value;
+
+        const users = getUsers();
+        users.push({ username: usernameV, email: emailV, password: passV });
+
+        localStorage.setItem("users", JSON.stringify(users));
+        window.location.href = "catalogue.html";
+
     } else {
         alert("Please enter correct data.");
     }
