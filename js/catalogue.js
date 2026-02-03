7dded15274d025fb865157d18308deb9f6ca93b3
ymatevva <107748308+ymatevva@users.noreply.github.com>
2026-02-03 20:49:45 +0000

    Deploying to gh-pages from @ ymatevva/MOVIE_VERSE@421ef5d4058a3510dc32b90cd81b523d14ec7f94 🚀

--- js/catalogue.js
+++ js/catalogue.js
@@ -98,11 +98,11 @@ addMovieForm.addEventListener('submit', e => {
     }
 
     const newMovie = {
-        title: title,
-        duration: document.getElementById("new-duration").value || "N/A",
-        releaseDate: document.getElementById("new-release").value, 
-        rating: document.getElementById("new-rating").value || "0",
-        director: document.getElementById("new-director").value || "N/A"
+        title,
+        duration: document.getElementById("new-duration").value,
+        releaseDate: document.getElementById("new-release").value,
+        rating: document.getElementById("new-rating").value,
+        director: document.getElementById("new-director").value
     };
 
     movies.push(newMovie);
